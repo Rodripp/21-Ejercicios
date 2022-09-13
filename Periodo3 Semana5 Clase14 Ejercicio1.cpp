5d9c25a94fcf0a177a0477d83d0942c27c899bda
Rodripp <113448635+Rodripp@users.noreply.github.com>
2022-09-13 08:46:33 -0600

    Create Periodo3 Semana5 Clase14 Ejercicio1.cpp

---
+++ Periodo3 Semana5 Clase14 Ejercicio1.cpp
@@ -0,0 +1,16 @@
+# incluir  < iostream >
+utilizando el espacio de  nombres  estándar ;
+
+int  principal (){
+	entero n = 5 ;
+	int i = 1 ;
+	mientras (yo <= 5 ) {
+		int j = 1 ;
+		mientras (j <= i) {
+			cout << j << "  " ;
+			j++;
+		}
+		cout << " \n " ;
+		yo++;
+	}
+}
