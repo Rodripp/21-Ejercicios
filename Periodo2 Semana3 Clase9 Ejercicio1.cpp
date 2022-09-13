6e77167badcb565b170ba21466cf9f6f7459da90
Rodripp <113448635+Rodripp@users.noreply.github.com>
2022-09-13 08:28:29 -0600

    Create Periodo2 Semana3 Clase9 Ejercicio1.cpp

---
+++ Periodo2 Semana3 Clase9 Ejercicio1.cpp
@@ -0,0 +1,23 @@
+# incluir < iostream >
+# incluir  < cmath >
+utilizando el espacio de  nombres  estándar ;
+
+flotar  pitágoras ( flotar , flotar );
+
+int  principal (){
+	float cat1,cat2,hipo= 0 ;
+	cout << " Ingrese cateto 1: " ;
+	cin>>cat1;
+	cout << " ingrese cateto 2: " ;
+	cin>>cat2;
+	hipo= pitágoras (cat1,cat2);
+	cout<< " La hipotenusa del triangulo es: " ;
+	cout << hipo;
+	devolver  0 ;
+}
+
+ pitágoras float ( float x, float y){
+	resultado flotante = 0 ;
+	resultado = sqrt ( pow (x, 2 )+ pow (y, 2 ));
+	resultado devuelto ;
+}
